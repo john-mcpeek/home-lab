2397ebec0cbabb8e4f929e974e1dd204b6cf578c
John McPeek <juan.mcpeek@gmail.com>
2025-09-11 20:32:17 -0400

    Use variables MY_PUBLIC_KEY and PROXMOX_ROOT_PUBLIC_KEY for dynamic SSH key configuration and NODE_IP for nameserver

--- vms/postgres/build-postgres-vm.sh
+++ vms/postgres/build-postgres-vm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export NODE_IP=$1
+
 qm shutdown 100
 qm destroy  100
 
@@ -15,7 +17,7 @@ qm set 100 --scsi2 local-lvm:32,ssd=1,discard=on,iothread=1,cache=none
 qm set 100 --cicustom "user=local:snippets/user-data-postgres.mime"
 qm set 100 --sshkey ~/.ssh/id_rsa.pub
 qm set 100 --ipconfig0 "ip=10.0.0.100/24,gw=10.0.0.1"
-qm set 100 --nameserver "10.0.0.10 8.8.8.8"
+qm set 100 --nameserver "${NODE_IP} 8.8.8.8"
 qm set 100 --tags postgres
 
 qm start 100
--- vms/postgres/generate-cloud-init-files.sh
+++ vms/postgres/generate-cloud-init-files.sh
@@ -3,13 +3,16 @@
 function build_postgres_vm_config() {
     local NODE_HOST_NAME=$1
 
-#    mkdir -p generated
     envsubst '${NODE_HOST_NAME}' < postgres/database.yaml | tee generated/database-${NODE_HOST_NAME}.yaml > /dev/null
 
     cloud-init devel make-mime \
      -a generated/database-${NODE_HOST_NAME}.yaml:cloud-config \
      > generated/user-data-${NODE_HOST_NAME}.mime
 }
+
+export MY_PUBLIC_KEY=$1
+PROXMOX_ROOT_PUBLIC_KEY=$(cat /root/.ssh/id_rsa.pub)
+export PROXMOX_ROOT_PUBLIC_KEY
 
 mkdir -p generated
 
